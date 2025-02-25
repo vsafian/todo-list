db13d64bb99840f1f2cf17e76fb1c13944a43698
Vitalii Safian <viisaf.prog.gt@gmail.com>
2025-02-25 17:51:34 +0200

    register 'app' urls to project urls

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("", include("app.urls", namespace="app")),
-]  + debug_toolbar_urls()
+
+]  + debug_toolbar_urls() + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
