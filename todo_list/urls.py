2dcc73a5197490196837e54c3010dee363dc3d47
Vitalii Safian <viisaf.prog.gt@gmail.com>
2025-02-26 12:21:48 +0200

    fix stylistic discrepancies in the code

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -14,15 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("", include("app.urls", namespace="app")),
-
-]  + debug_toolbar_urls() + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("accounts/", include("django.contrib.auth.urls")),
+        path("", include("app.urls", namespace="app")),
+    ]
+    + debug_toolbar_urls()
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
